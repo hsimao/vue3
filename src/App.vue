c542a7ecf1b4bdeefeea8d256db31e8a8fbc127c
hsimao <e087754958@gmail.com>
2020-01-01 13:25:17 +0800

    add: 封裝滑鼠座標監聽方法

--- src/App.vue
+++ src/App.vue
@@ -6,53 +6,67 @@
     </div>
     <ProductForm :createProduct="createProduct" />
     <Products :items="products" :remove="deleteProduct" />
+    滑鼠x: {{mouseX}}<br>
+    滑鼠y: {{mouseY}}
   </div>
 </template>
 
 <script>
-import ProductForm from './components/ProductForm.vue'
-import Products from './components/Producets'
-import { ref } from '@vue/composition-api'
-import { useToggle } from './cmp-functions/toggle'
+import ProductForm from "./components/ProductForm.vue";
+import Products from "./components/Producets";
+import { ref } from "@vue/composition-api";
+import { useToggle } from "./cmp-functions/toggle";
+import { useMousePosition } from "./cmp-functions/mousePosition";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     ProductForm,
-    Products,
+    Products
   },
   setup() {
     // 使用 setup 管理變數與 function 的好處, 可以統一歸類擺放
     // 高複用的邏輯可非常容易看出, 並抽出封裝, 例如以下 useToggle
 
     // 宣告響應式變數：使用 ref
-    const products = ref([])
+    const products = ref([]);
 
     // 宣告 function
     const createProduct = (title, price) => {
       const newProduct = {
         id: Math.random(),
         title,
-        price,
-      }
-      products.value.push(newProduct)
-    }
+        price
+      };
+      products.value.push(newProduct);
+    };
 
     const deleteProduct = productId => {
-      products.value = products.value.filter(p => p.id !== productId)
-    }
+      products.value = products.value.filter(p => p.id !== productId);
+    };
 
     // 使用已經封裝好的 toggle, 並自訂變數名稱與方法名稱, 不會有原本 mixins 命名衝突的問題
-    const { show: showUserInfo, toggle: toggleUserInfo } = useToggle()
+    const { show: showUserInfo, toggle: toggleUserInfo } = useToggle();
 
-    return { products, showUserInfo, createProduct, deleteProduct, toggleUserInfo }
-  },
-}
+    // 使用已經封裝好的 useMousePosition, 取得當前滑鼠座標
+    const { x: mouseX, y: mouseY } = useMousePosition();
+
+    return {
+      products,
+      showUserInfo,
+      createProduct,
+      deleteProduct,
+      toggleUserInfo,
+      mouseX,
+      mouseY
+    };
+  }
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
